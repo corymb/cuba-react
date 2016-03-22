9d437c61003a88c7c9b10399746dbfd17ee0150f
Dave Bush <github@david-bush.co.uk>
2016-03-22 07:30:50 +0000

    Added loader method.

--- lib/cuba_react.rb
+++ lib/cuba_react.rb
@@ -1,4 +1,12 @@
+require 'opal'
 require "cuba_react/version"
 
 module CubaReact
+  def loader
+    opal = Opal::Server.new {|s|
+      s.append_path 'js'
+      s.main = 'react'
+    }
+    Opal::Processor.load_asset_code(opal.sprockets, 'react')
+  end
 end
